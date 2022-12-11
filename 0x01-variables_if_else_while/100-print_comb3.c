ef345fece7d8ecbcba1a9b892870dedb69b41ae7
yurijames <yurijamesmail@gmail.com>
2022-12-11 13:48:00 +0300

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0 ; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			if (i < j)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
 
-				if (i != '8' || (i == '8' && j != '9'))
+				if (i != 8 || (i == 8 && j != 9))
 			{
 				putchar(',');
 				putchar(' ');
