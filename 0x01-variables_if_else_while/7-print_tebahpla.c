af29695a8d947f5b3520a33991988a005f92a4ad
yurijames <yurijamesmail@gmail.com>
2022-12-11 11:43:07 +0300

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main: Entry point
+ * Description: letters in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
