1956cb96bab7fc93160a52fd30bb9d881656ac31
yurijames <yurijamesmail@gmail.com>
2022-12-12 12:59:16 +0300

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * Description: program that prints all possible different combinations of three digits
+ * Description: program that prints all possible combinations of three digits
  * Return: 0 (Success)
  */
 int main(void)
 {
-	int i ,j ,k;
+	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
 	{
@@ -15,7 +15,7 @@ int main(void)
 		{
 			for (k = 0; k <= 9; k++)
 			{
-				if ( i < j && j < k)
+				if (i < j && j < k)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
