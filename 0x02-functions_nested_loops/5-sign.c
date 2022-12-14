28ef2f3534a006cba22c979edde31b1a7c401d40
Yuri <yurijamesmail@gmail.com>
2022-12-14 13:44:30 +0300

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * @n: integer to print sign
+ * Description: Prints sign of numbers
+ * Return: 0, 1 or -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		_putchar('-');
+	}
+	return (0);
+}
