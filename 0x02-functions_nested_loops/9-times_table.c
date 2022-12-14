fde8dd52820745d4e5e2110aebd1d39c4be57598
Yuri <yurijamesmail@gmail.com>
2022-12-14 16:27:59 +0300

    10-add

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ * Description: A pogram for 9 times tables
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		_putchar('0' + '0');
+ 
+	}
+}
