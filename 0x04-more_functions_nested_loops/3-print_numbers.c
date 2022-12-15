75248eeb1192f573fa8622c7332e238661a63279
Yuri <yurijamesmail@gmail.com>
2022-12-15 13:59:24 +0300

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
