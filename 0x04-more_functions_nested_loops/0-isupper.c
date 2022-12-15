7d6773b3fd3a906c14f0efe43955fcfed7cb7721
Yuri <yurijamesmail@gmail.com>
2022-12-15 12:42:47 +0300

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * Description: Check for uppercase letters
+ * Return: 1 if upper, 0 if otherwise
+ * @c: character for function
+ */
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (c);
+
+}
