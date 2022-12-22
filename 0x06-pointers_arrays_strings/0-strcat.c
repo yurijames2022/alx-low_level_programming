7db2ab501554380c4fbeaecef1b39f17a2e14422
yurijames <yurijamesmail@gmail.com>
2022-12-22 22:54:47 +0300

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcat - Entry point
+ * @src: source
+ * @dest: destiation 
+ * Return: Return pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int dl = 0;
+
+	while (dest[i++])
+	{
+		dl++;
+	}
+	for (i = 0; src[i]; i++)
+	{
+		dest[dl++] = src[i];
+	}
+	return (dest);
+}
