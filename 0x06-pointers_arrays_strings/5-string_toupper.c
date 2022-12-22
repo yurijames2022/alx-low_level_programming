2c00cd4e113ee01dbf2cfb3e82b2755bbbead8f4
yurijames <yurijamesmail@gmail.com>
2022-12-22 23:42:33 +0300

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - Entry point
+ * @str: The string to be changed.
+ * Return: A pointer to the changed string.
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		i++;
+	}
+
+	return (str);
+}
