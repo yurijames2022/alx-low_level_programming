c0332088e871664122fb533054f3b8ab1f8fd36d
Yuri <yurijamesmail@gmail.com>
2023-01-12 10:44:33 +0300

    Create Array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,23 +13,20 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ptr;
 
-	ptr = malloc(sizeof(char) * size);
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-
-	for (i = 0; i < size; i++)
-	{
-		ptr[i] = c;
-
-	}
-
+	ptr = malloc(sizeof(char) * size);
 
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
+
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+
 	return (ptr);
 }
