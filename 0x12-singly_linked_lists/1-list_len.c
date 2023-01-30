00e5a7c5105242c189f71ce2b66806c71292ffb9
yurijames <yurijamesmail@gmail.com>
2023-01-30 17:01:10 +0300

    b

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 
 /**
  * list_len - Returns no. of elemrnts in a linked list
- * @h - List
+ * @h: List
  * Return: The number of elements
  */
 
@@ -11,7 +11,7 @@ size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
-	while(h)
+	while (h)
 	{
 		count++;
 		h = h->next;
