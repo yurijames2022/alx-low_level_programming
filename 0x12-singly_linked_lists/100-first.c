97c4343b1c937709520c4d36cfd092c592cba5e0
yurijames <yurijamesmail@gmail.com>
2023-01-31 13:33:22 +0300

    attribute constructor

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ * before_print - Prints before main executes
+ */
 void before_print(void) __attribute__ ((constructor));
 
 void before_print(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
