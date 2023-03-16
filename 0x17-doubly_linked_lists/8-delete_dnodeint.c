3889205f0e355cac19b39505bb970f4063ab6f6c
Jack Yuri <yurijamesmail@gmail.com>
2023-03-16 13:50:52 +0000

    del

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,45 +10,40 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *current = *head;
 	unsigned int i;
-	dlistint_t *temp, *temp1, *temp2;
-
-	temp = *head;
 
 	if (*head == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 
 	if (index == 0)
 	{
-		*head = temp->next;
+		*head = current->next;
 		if (*head != NULL)
 		{
 			(*head)->prev = NULL;
 		}
-		free(temp);
+		free(current);
+		return (1);
 	}
-	else
-	{
-		while (temp != NULL && i < index)
-		{
-			temp = temp->next;
-			i++;
-		}
-		if (temp == NULL)
-		{
-			return (-1);
-		}
-		temp1 = temp->prev;
-		temp2 = temp->next;
-		temp1->next = temp2;
-		if (temp2 != NULL)
-		{
-			temp2->prev = temp1;
-		}
-		free(temp);
 
+	for (i = 0; i < index && current != NULL; i++)
+	{
+		current = current->next;
 	}
+
+	if (current == NULL)
+	{
+		return (-1);
+	}
+
+	current->prev->next = current->next;
+	if (current->next != NULL)
+	{
+		current->next->prev = current->prev;
+	}
+	free(current);
 	return (1);
 }
