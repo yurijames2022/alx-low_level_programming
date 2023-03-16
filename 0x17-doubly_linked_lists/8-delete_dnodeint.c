51d1095f1847393ee049a52a8513ba9349188167
Jack Yuri <yurijamesmail@gmail.com>
2023-03-16 13:24:00 +0000

    del

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,6 +15,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	temp = *head;
 
+	if (*head == NULL)
+	{
+		return -1;
+	}
+
 	if (index == 0)
 	{
 		*head = (*head)->next;
